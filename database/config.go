c483400601f2d5a25e8ed9c6fc45deae94e8711f
Kauã Matheus <kauamatheussilva06@gmail.com>
2025-10-23 17:47:20 -0300

    feat: Changes the database table format

--- database/config.go
+++ database/config.go
@@ -8,10 +8,11 @@ import (
 
 type SensorData struct {
 	ID					uuid.UUID	`json:"id" gorm:"type:uuid;primaryKey"`
+	SensorID			string		`json:"sensor_id"`
 	Temperature			float32		`json:"temperature"`
-	General_Humidity	float32		`json:"general_humidity"`
+	Humidity			float32		`json:"humidity"`
 	Soil_Moisture		float32		`json:"soil_moisture"`
-	Luminosity			float32		`json:"luminosity"`
+	Light_Level			float32		`json:"light_level"`
 	CreatedAt	        time.Time 	`json:"created_at" gorm:"autoCreateTime"`
     UpdatedAt			time.Time 	`json:"updated_at" gorm:"autoUpdateTime"`
 }
@@ -24,7 +25,7 @@ func (s *SensorData) BeforeCreate(tx *gorm.DB) (err error) {
 }
 
 func (SensorData) TableName() string {
-	return "sensor_data"
+	return "glp_data"
 }
 
 type DataProvider interface {
